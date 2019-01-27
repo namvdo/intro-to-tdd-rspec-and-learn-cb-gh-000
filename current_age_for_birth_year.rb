6a7ebaf1e34ff23fb577255fa50ebc338e883e4d
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-27 09:04:40 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,4 @@
 def current_age_for_birth_year(birth_year)
-  age_of_person = current_age_for_birth_year(1984)
-
-  expect(age_of_person).to eq(19)
-  
+  2003 - birth_year
 end
+current_age_for_birth_year(1984);
